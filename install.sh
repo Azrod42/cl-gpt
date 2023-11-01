fc6c484d56b5c45c958edab8f24e515b34597ab0
tsorabel <tom.sorabella@gmail.com>
2023-11-01 14:12:55 +0100

    fix: update install script

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sed -i "s/YOUR_API_KEY/$OPEN_AI_KEY/g" src/api.rs
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo build --release
 cp target/release/gpt ~/.config
-echo 'gpt="~/.config/gpt \"\""' >> ~/.zshrc
+echo 'alias gpt="~/.config/gpt \"\""' >> ~/.zshrc

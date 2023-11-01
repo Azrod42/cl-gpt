8b149dcec92e47b67e02b2004f0374c29e357242
Azrod <109629380+Azrod42@users.noreply.github.com>
2023-11-01 15:05:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #sed -i "s/YOUR_API_KEY/$OPEN_AI_KEY/g" src/api.rs
-perl -pi -e 's/YOUR_API_KEY/$OPEN_AI_KEY/g' src/api.rs
+perl -pi -e "s/YOUR_API_KEY/$OPEN_AI_KEY/g" src/api.rs
 cargo build --release
 cp target/release/gpt ~/.config
 echo 'alias gpt="~/.config/gpt \"\""' >> ~/.zshrc

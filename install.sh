9ed0ffc4d1ff11c90cd7a8a3b4afdf381b78fa30
tsorabel <tom.sorabella@gmail.com>
2023-11-01 14:06:30 +0100

    feat: add install .sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+sed -i "s/YOUR_API_KEY/$OPEN_AI_KEY/g" src/api.rs
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo build --release
+cp target/release/gpt ~/.config
+echo 'gpt="~/.config/gpt \"\""' >> ~/.zshrc
